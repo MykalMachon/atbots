d50056ff7795ad304c02d27d26d4b1c6429d3b16
Mykal Machon <mykal@tinybox.dev>
2024-11-02 23:43:20 -0700

    feat: roll dice function
    adding some toy behaviours

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import { Bot, IncomingChatPreference } from "@skyware/bot";
 import dotenv from 'dotenv';
 
+/**
+ * rolls a dice and returns the result
+ * 
+ * @param {number} sides: The number of sides on the dice 
+ */
+const rollDice = (sides = 6) => {
+  return Math.floor(Math.random() * sides) + 1;
+}
+
 const startup = async () => {
   dotenv.config();
   const bot = new Bot({ emitChatEvents: true });
@@ -35,10 +44,20 @@ const startup = async () => {
     const sender = await message.getSender();
     console.log(`Received message from ${sender.handle}: ${message.text}`);
 
+    let responseText;
+
+    if (message.text.toLowerCase().includes('roll')) {
+      const sides = parseInt(message.text.split(' ')[1]) || 6;
+      const result = rollDice(sides);
+      responseText = `You rolled a ${result}!`
+    } else {
+      responseText = `I'm sorry, I don't understand what you mean. You can ask me to roll a dice by saying "roll" followed by the number of sides.`
+    }
+
     const conversation = await message.getConversation();
     if (conversation) {
       // conversations may not always work
-      await conversation.sendMessage({ text: `Hey ${sender.displayName}! I can't do anything yet. But I'm learning!` });
+      await conversation.sendMessage({ text: responseText });
     }
   })
 }
